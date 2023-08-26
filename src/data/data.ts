49389a9171d0bce88716e3440eff3f6fa93b4cdc
PeterBroom <harleybroom@googlemail.com>
2023-08-26 21:23:31 +0100

    1 file modified

--- src/data/data.ts
+++ src/data/data.ts
@@ -12,7 +12,7 @@ export const data: ComponentBlocks = {
     },
     {
       componentType: 'MESSAGE_COMPONENT',
-      text: 'This article discusses the code utilised in crafting the content of this article.'
+      text: 'This article discusses the code utilised in crafting the content of this article to demonstrate how maybe patterns and type guards can help prevent errors in the absence of data.'
     },
     {
       componentType: 'MESSAGE_COMPONENT',
